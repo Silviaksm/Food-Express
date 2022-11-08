f698945a54d8cc30176751fdb39299a174cedcf5
Silviaksm <sksalazarm@gmail.com>
2022-11-07 23:12:41 -0600

    Cree archivos de componentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // falta importar switch y router 
 function App() {
   return (
     <BrowserRouter>
-
       <div className="App">
-       
-          <h1>Foot Express</h1>
-     
+        
+          <h1>Food Express</h1>
+        
       </div>
-
     </BrowserRouter>
   );
 
